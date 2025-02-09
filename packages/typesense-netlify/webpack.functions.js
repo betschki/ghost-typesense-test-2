ac146e4465dee5d59ec547a373b8ec1f7e3f483b
Jannis Fedoruk-Betschki <jannis@magicpages.co>
2025-02-09 11:16:34 +0100

    Upgrade Node.js to version 22 and update webpack configuration for improved compatibility

--- packages/typesense-netlify/webpack.functions.js
+++ packages/typesense-netlify/webpack.functions.js
@@ -3,6 +3,16 @@ module.exports = {
         fallback: {
             "stream": require.resolve("stream-browserify"),
             "buffer": require.resolve("buffer/")
+        },
+        alias: {
+            'node:stream': 'stream-browserify'
         }
-    }
+    },
+    plugins: [
+        // Add Node.js polyfills
+        new (require('webpack')).ProvidePlugin({
+            process: 'process/browser',
+            Buffer: ['buffer', 'Buffer']
+        })
+    ]
 }; 
